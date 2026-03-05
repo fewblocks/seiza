9250ae45f98000507243c666b9169c2470ae14ef
fewblocks <o.w.f.n.life@gmail.com>
2026-03-05 23:45:12 +0900

    faviconのLocationヘッダーをbaseパスに基づいて更新

--- src/routes/favicon.ico/+server.ts
+++ src/routes/favicon.ico/+server.ts
@@ -1,10 +1,12 @@
 export const prerender = true;
 
+import { base } from '$app/paths';
+
 export function GET() {
 	return new Response(null, {
 		status: 302,
 		headers: {
-			Location: '/favicon.svg'
+			Location: base + '/favicon.svg'
 		}
 	});
 }
